543f811654ae872260cdb158c6c61af9d5df9588
Sohaib-DigitalOcean-Ubuntu20 <sohaibm131@gmail.com>
2021-05-26 02:53:15 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+// Constants - express init
 const express = require('express');
 const app = express()
 
+// Constants - connection spec
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// Simple hello world response
 app.get('/', (req, res) => res.send('hello world'));
 
-app.listen(3000, () => {
-  console.log('Hello World REST API running on port 3000')
+app.listen(PORT, () => {
+  console.log('Hello World REST API running on http://${HOST}:${PORT}')
 });
