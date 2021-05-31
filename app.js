41a4c5e544dc70622fc805beb328ac44b95236b8
Sohaib-DigitalOcean-Ubuntu20 <sohaibm131@gmail.com>
2021-05-31 03:05:58 +0000

    Added index.html

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Constants - express init
 const express = require('express');
+const path = require('path')
 const app = express()
 
 // Constants - connection spec
@@ -7,7 +8,9 @@ const PORT = 3000;
 const HOST = '0.0.0.0';
 
 // Simple hello world response
-app.get('/', (req, res) => res.send('hello world'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Hello World REST API running on http://${HOST}:${PORT}`)
